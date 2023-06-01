c3b2174cfc190c7931963e4b787168f7d94e13ac
nuno <hi@nunoalexandre.com>
2023-06-01 10:59:47 +0200

    Fix tests

--- tests/xcm-transfer/kusama-relay.test.ts
+++ tests/xcm-transfer/kusama-relay.test.ts
@@ -8,64 +8,64 @@ import { altair } from '../../networks/centrifuge'
 import buildTest from './shared'
 
 const tests = [
-  // // karura <-> kusama
-  // {
-  //   from: 'karura',
-  //   to: 'kusama',
-  //   name: 'KSM',
-  //   test: {
-  //     xtokensUp: {
-  //       tx: tx.xtokens.transfer(karura.ksm, 1e12, tx.xtokens.relaychainV2),
-  //       balance: query.tokens(karura.ksm),
-  //     },
-  //   },
-  // },
-  // {
-  //   from: 'kusama',
-  //   to: 'karura',
-  //   name: 'KSM',
-  //   test: {
-  //     xcmPalletDown: {
-  //       tx: tx.xcmPallet.limitedReserveTransferAssetsV3(kusama.ksm, 1e12, tx.xcmPallet.parachainV3(0, karura.paraId)),
-  //       balance: query.tokens(karura.ksm),
-  //     },
-  //   },
-  // },
-  // // karura2180 <-> kusama
-  // {
-  //   from: 'karura2180',
-  //   to: 'kusama',
-  //   name: 'KSM',
-  //   test: {
-  //     xtokensUp: {
-  //       tx: tx.xtokens.transfer(karura.ksm, 1e12, tx.xtokens.relaychainV3),
-  //       balance: query.tokens(karura.ksm),
-  //     },
-  //   },
-  // },
-  // {
-  //   from: 'kusama',
-  //   to: 'karura2180',
-  //   name: 'KSM',
-  //   test: {
-  //     xcmPalletDown: {
-  //       tx: tx.xcmPallet.limitedReserveTransferAssetsV3(kusama.ksm, 1e12, tx.xcmPallet.parachainV3(0, karura.paraId)),
-  //       balance: query.tokens(karura.ksm),
-  //     },
-  //   },
-  // },
-  // // kusama <-> shiden
-  // {
-  //   from: 'kusama',
-  //   to: 'shiden',
-  //   name: 'KSM',
-  //   test: {
-  //     xcmPalletDown: {
-  //       tx: tx.xcmPallet.limitedReserveTransferAssetsV3(kusama.ksm, 1e12, tx.xcmPallet.parachainV3(0, shiden.paraId)),
-  //       balance: query.assets(shiden.ksm),
-  //     },
-  //   },
-  // },
+  // karura <-> kusama
+  {
+    from: 'karura',
+    to: 'kusama',
+    name: 'KSM',
+    test: {
+      xtokensUp: {
+        tx: tx.xtokens.transfer(karura.ksm, 1e12, tx.xtokens.relaychainV2),
+        balance: query.tokens(karura.ksm),
+      },
+    },
+  },
+  {
+    from: 'kusama',
+    to: 'karura',
+    name: 'KSM',
+    test: {
+      xcmPalletDown: {
+        tx: tx.xcmPallet.limitedReserveTransferAssetsV3(kusama.ksm, 1e12, tx.xcmPallet.parachainV3(0, karura.paraId)),
+        balance: query.tokens(karura.ksm),
+      },
+    },
+  },
+  // karura2180 <-> kusama
+  {
+    from: 'karura2180',
+    to: 'kusama',
+    name: 'KSM',
+    test: {
+      xtokensUp: {
+        tx: tx.xtokens.transfer(karura.ksm, 1e12, tx.xtokens.relaychainV3),
+        balance: query.tokens(karura.ksm),
+      },
+    },
+  },
+  {
+    from: 'kusama',
+    to: 'karura2180',
+    name: 'KSM',
+    test: {
+      xcmPalletDown: {
+        tx: tx.xcmPallet.limitedReserveTransferAssetsV3(kusama.ksm, 1e12, tx.xcmPallet.parachainV3(0, karura.paraId)),
+        balance: query.tokens(karura.ksm),
+      },
+    },
+  },
+  // kusama <-> shiden
+  {
+    from: 'kusama',
+    to: 'shiden',
+    name: 'KSM',
+    test: {
+      xcmPalletDown: {
+        tx: tx.xcmPallet.limitedReserveTransferAssetsV3(kusama.ksm, 1e12, tx.xcmPallet.parachainV3(0, shiden.paraId)),
+        balance: query.assets(shiden.ksm),
+      },
+    },
+  },
 
   // kusama <-> altair
   {
@@ -86,7 +86,7 @@ const tests = [
     test: {
       xtokensUp: {
         tx: tx.xtokens.transfer(altair.ksm, 1e12, tx.xtokens.relaychainV2),
-        balance: query.ormlTokens(kusama.ksm),
+        balance: query.ormlTokens(altair.ksm),
       },
     },
   },
